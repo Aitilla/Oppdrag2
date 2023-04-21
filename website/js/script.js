000c017e2e4c48a74a44b2b1d7056e085e931360
Aitilla <jhertaas@gmail.com>
2023-04-21 09:26:31 +0200

    feilet

--- website/js/script.js
+++ website/js/script.js
@@ -1,28 +1,31 @@
+// Henter og lager alle variabler som skal brukes
 const bodyColor = document.getElementById('bodyColor');
 const blindness = document.getElementById('blindness');
+const changeColor = document.getElementById('changeColor');
 const classes = ['default', 'protanopia', 'deuteranopia', 'tritanopia'];
 const names = ['Normal', 'Protanopia', 'Deuteranopia', 'Tritanopia'];
 let currentIndex = 0;
-const value = document.getElementById['option1', 'option2', 'option3', 'option4']
 console.log(currentIndex);
 
-function changeColor() {
-
+// Bruker en addEventListener istedet for onclick for å endre på nettsiden til tilsvarende farge
+changeColor.addEventListener('click', function() {
     bodyColor.classList.remove(classes[currentIndex]);
     currentIndex = (currentIndex + 1) % classes.length;
     bodyColor.classList.add(classes[currentIndex]);
     blindness.innerHTML = names[currentIndex];
     console.log(currentIndex);
+});
 
-}
-
+// Henter og lager alle variabler som skal brukes
 const optDiv = document.getElementById("optDiv");
 const ulEl = document.createElement("ul");
 const textarea = document.getElementById('textarea');
+// Logger alt som blir skrevet
 textarea.addEventListener('input', function() {
     console.log(textarea.value);
 });
 
+// Legger til alt som blir skrevet i textarea
 const addText = document.getElementById('addText');
 addText.addEventListener('click', function() {
     if (textarea.value === ''){
@@ -43,6 +46,3 @@ addText.addEventListener('click', function() {
         });
         }
 });
-
-
-
